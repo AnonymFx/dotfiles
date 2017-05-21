287de6e83d2077d7d31eb2fa993d979f251be9f6
Thomas Pettinger <pettinger.thomas@gmail.com>
2017-05-21 13:19:38 +0200

    showing secondary polybar on all monitors but primary one

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,8 +7,11 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
+# Get primary monitor and set environment variable to load the primary bar on this monitor
 MONITOR="$(xrandr -q | grep -e 'primary' | cut -d " " -f1 | head -n 1)" polybar bar1 &
-MONITOR="$(xrandr -q | grep -e ' connected' | grep -v 'primary' | cut -d ' ' -f1 | sed '1q;d')" polybar bar2 &
-MONITOR="$(xrandr -q | grep -e ' connected' | grep -v 'primary' | cut -d ' ' -f1 | sed '2q;d')" polybar bar3 &
+# Show the default bar for other monitors connected
+while read -r monitor; do
+    MONITOR=$monitor polybar bar2 &
+done <<< "$(xrandr -q | grep -e ' connected' | grep -v 'primary' | cut -d ' ' -f1)"
 
 echo "Bars launched..."
