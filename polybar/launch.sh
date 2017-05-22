cce8469b77e24b6b1df22533f51ab71370cc85f5
Thomas Pettinger <pettinger.thomas@gmail.com>
2017-05-22 18:19:11 +0200

    adapted polybar to work with different eth, wlan, and battery adapters

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,10 +8,16 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 # Get primary monitor and set environment variable to load the primary bar on this monitor
-MONITOR="$(xrandr -q | grep -e 'primary' | cut -d " " -f1 | head -n 1)" polybar bar1 &
+WLAN="$(ls /sys/class/net | grep wlp)" \
+ETH="$(ls /sys/class/net | grep enp)" \
+BAT="$(ls /sys/class/power_supply | grep BAT)" \
+BAT_ADP="$(ls /sys/class/power_supply | grep A)" \
+MONITOR="$(xrandr -q | grep -e 'primary' | cut -d " " -f1 | head -n 1)" \
+polybar bar1 &
 # Show the default bar for other monitors connected
 while read -r monitor; do
-    MONITOR=$monitor polybar bar2 &
+    MONITOR=$monitor \
+    polybar bar2 &
 done <<< "$(xrandr -q | grep -e ' connected' | grep -v 'primary' | cut -d ' ' -f1)"
 
 echo "Bars launched..."
