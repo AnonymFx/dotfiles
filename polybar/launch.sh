984d55f73968e8ec210adea1ce6e79ea2de57e73
Thomas Pettinger <pettinger.thomas@gmail.com>
2017-06-09 22:53:41 +0200

    renamed polybars

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,18 +6,18 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
+# Launch primarybar and secondarybar
 # Get primary monitor and set environment variable to load the primary bar on this monitor
 WLAN="$(ls /sys/class/net | grep wl)" \
 ETH="$(ls /sys/class/net | grep en)" \
 BAT="$(ls /sys/class/power_supply | grep BAT)" \
 BAT_ADP="$(ls /sys/class/power_supply | grep A)" \
 MONITOR="$(xrandr -q | grep -e 'primary' | cut -d " " -f1 | head -n 1)" \
-polybar bar1 &
+polybar primarybar &
 # Show the default bar for other monitors connected
 while read -r monitor; do
     MONITOR=$monitor \
-    polybar bar1 &
+    polybar secondarybar &
 done <<< "$(xrandr -q | grep -e ' connected' | grep -v 'primary' | cut -d ' ' -f1)"
 
 echo "Bars launched..."
