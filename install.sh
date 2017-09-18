7fad6e936bebed89e21f4402ab6062815525a5a5
Thomas Pettinger <pettinger.thomas@gmail.com>
2017-09-18 10:12:02 +0200

    install: autoformat

--- install.sh
+++ install.sh
@@ -3,29 +3,29 @@ function print_help_msg() {
 	cat <<-EOF
 	Usage: install.sh TARGET
 
-    TARGET := { all,
-                nogui
-                autorandr
-                dconf
-                bash
-                gdb
-                git
-                gtk
-                i3-gaps
-                ideavim
-                polybar
-                ranger
-                readline
-                terminator
-                tmux
-                vim
-                neovim
-                vimiv
-                vrapper
-                zathura
-                zsh
-                X
-              }
+	TARGET := { all,
+				nogui
+				autorandr
+				dconf
+				bash
+				gdb
+				git
+				gtk
+				i3-gaps
+				ideavim
+				polybar
+				ranger
+				readline
+				terminator
+				tmux
+				vim
+				neovim
+				vimiv
+				vrapper
+				zathura
+				zsh
+				X
+			}
 EOF
 }
 
@@ -152,101 +152,101 @@ function post_install() {
 }
 
 function link_config() {
-    local TARGET="$1"
+	local TARGET="$1"
 
 	echo "Linking config files for $TARGET"
 
-    case "$TARGET" in
-        autorandr )
+	case "$TARGET" in
+		autorandr )
 			mkdir -p $HOME/.config/autorandr
-            ln -snf $PWD/autorandr/postswitch $HOME/.config/autorandr/postswitch
-            ;;
-        bash )
-            ln -snf $PWD/bash/bashrc $HOME/.bashrc
-            ;;
-        dconf )
-            dconf load / < $PWD/dconf/keybindings
-            ;;
-        gdb )
-            ln -snf $PWD/gdb/gdbinit $HOME/.gdbinit
-            ln -snf $PWD/gdb/gdbinit.d $HOME/.gdbinit.d
-            ;;
-        git )
-            ln -snf $PWD/git/global_gitignore $HOME/.gitignore_global
+			ln -snf $PWD/autorandr/postswitch $HOME/.config/autorandr/postswitch
+			;;
+		bash )
+			ln -snf $PWD/bash/bashrc $HOME/.bashrc
+			;;
+		dconf )
+			dconf load / < $PWD/dconf/keybindings
+			;;
+		gdb )
+			ln -snf $PWD/gdb/gdbinit $HOME/.gdbinit
+			ln -snf $PWD/gdb/gdbinit.d $HOME/.gdbinit.d
+			;;
+		git )
+			ln -snf $PWD/git/global_gitignore $HOME/.gitignore_global
 			ln -snf $PWD/git/gitconfig $HOME/.gitconfig
-            git config --global core.excludesfile $HOME/.gitignore_global
-            ;;
-        gtk )
+			git config --global core.excludesfile $HOME/.gitignore_global
+			;;
+		gtk )
 			mkdir -p $HOME/.config/gtk-3.0
-            ln -snf $PWD/gtk/gtkrc-2.0 $HOME/.gtkrc-2.0
-            ln -snf $PWD/gtk/gtk-3.0-settings.ini $HOME/.config/gtk-3.0/settings.ini
-            ;;
-        i3-gaps )
+			ln -snf $PWD/gtk/gtkrc-2.0 $HOME/.gtkrc-2.0
+			ln -snf $PWD/gtk/gtk-3.0-settings.ini $HOME/.config/gtk-3.0/settings.ini
+			;;
+		i3-gaps )
 			mkdir -p $HOME/.config/i3
 			mkdir -p $HOME/Pictures
-            ln -snf $PWD/i3-gaps/config $HOME/.config/i3/config
-            ln -snf $PWD/i3-gaps/background.png $HOME/Pictures/i3-bg.png
-            ;;
-        ideavim )
-            ln -snf $PWD/ideavim/ideavimrc $HOME/.ideavimrc
-            ;;
-        polybar )
+			ln -snf $PWD/i3-gaps/config $HOME/.config/i3/config
+			ln -snf $PWD/i3-gaps/background.png $HOME/Pictures/i3-bg.png
+			;;
+		ideavim )
+			ln -snf $PWD/ideavim/ideavimrc $HOME/.ideavimrc
+			;;
+		polybar )
 			mkdir -p $HOME/.config/polybar
-            ln -snf $PWD/polybar/config $HOME/.config/polybar/config
-            ln -snf $PWD/polybar/launch.sh $HOME/.config/polybar/launch.sh
-            ;;
-        ranger )
+			ln -snf $PWD/polybar/config $HOME/.config/polybar/config
+			ln -snf $PWD/polybar/launch.sh $HOME/.config/polybar/launch.sh
+			;;
+		ranger )
 			mkdir -p $HOME/.config/ranger
-            ln -snf $PWD/ranger/rifle.conf $HOME/.config/ranger/rifle.conf
-            ;;
-        readline )
-            ln -snf $PWD/readline/inputrc $HOME/.inputrc
-            ;;
-        terminator )
-            mkdir -p $HOME/.config/terminator
-            ln -snf $PWD/terminator/terminator.conf $HOME/.config/terminator/config
-            ;;
-        tmux )
-            ln -snf $PWD/tmux/tmux.conf $HOME/.tmux.conf
-            ln -snf $PWD/tmux/tmux.layouts $HOME/.tmux.layouts
-            ;;
-        vim )
-            mkdir -p $HOME/.vim
-            ln -snf $PWD/vim/vim/autoload $HOME/.vim/autoload
-            ln -snf $PWD/vim/vimrc $HOME/.vimrc
-            ln -snf $PWD/vim/gvimrc $HOME/.gvimrc
-            ;;
-        neovim )
+			ln -snf $PWD/ranger/rifle.conf $HOME/.config/ranger/rifle.conf
+			;;
+		readline )
+			ln -snf $PWD/readline/inputrc $HOME/.inputrc
+			;;
+		terminator )
+			mkdir -p $HOME/.config/terminator
+			ln -snf $PWD/terminator/terminator.conf $HOME/.config/terminator/config
+			;;
+		tmux )
+			ln -snf $PWD/tmux/tmux.conf $HOME/.tmux.conf
+			ln -snf $PWD/tmux/tmux.layouts $HOME/.tmux.layouts
+			;;
+		vim )
+			mkdir -p $HOME/.vim
+			ln -snf $PWD/vim/vim/autoload $HOME/.vim/autoload
+			ln -snf $PWD/vim/vimrc $HOME/.vimrc
+			ln -snf $PWD/vim/gvimrc $HOME/.gvimrc
+			;;
+		neovim )
 			mkdir -p $HOME/.config/nvim
-            ln -snf $PWD/vim/vim $HOME/.config/nvim
-            ln -snf $PWD/vim/vimrc $HOME/.config/nvim/init.vim
-            ;;
-        vimiv )
-            ln -snf $PWD/vimiv $HOME/.vimiv
-            ;;
-        vrapper )
-            ln -snf $PWD/vrapper/vrapperrc $HOME/.vrapperrc
-            ;;
-        zathura )
+			ln -snf $PWD/vim/vim $HOME/.config/nvim
+			ln -snf $PWD/vim/vimrc $HOME/.config/nvim/init.vim
+			;;
+		vimiv )
+			ln -snf $PWD/vimiv $HOME/.vimiv
+			;;
+		vrapper )
+			ln -snf $PWD/vrapper/vrapperrc $HOME/.vrapperrc
+			;;
+		zathura )
 			mkdir -p $HOME/.config/zathura
-            ln -snf $PWD/zathura/zathurarc $HOME/.config/zathura/zathurarc
-            ;;
-        zsh )
-            ln -snf $PWD/zsh/zshrc $HOME/.zshrc
-            ;;
-        X )
+			ln -snf $PWD/zathura/zathurarc $HOME/.config/zathura/zathurarc
+			;;
+		zsh )
+			ln -snf $PWD/zsh/zshrc $HOME/.zshrc
+			;;
+		X )
 			mkdir -p $HOME/.config
-            ln -snf $PWD/X/Xresources $HOME/.Xresources
-            ln -snf $PWD/X/mimeapps.list $HOME/.config/mimeapps.list
-            ;;
-        esac
+			ln -snf $PWD/X/Xresources $HOME/.Xresources
+			ln -snf $PWD/X/mimeapps.list $HOME/.config/mimeapps.list
+			;;
+	esac
 }
 
 function install_additional() {
-    local TARGET="$1"
+	local TARGET="$1"
 
-    case "$TARGET" in
-        zsh )
+	case "$TARGET" in
+		zsh )
 			# Oh-My-Zsh
 			echo "Installing oh-my-zsh"
 			sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -273,12 +273,12 @@ function install_additional() {
 			# spaceship theme
 			echo "Installing/Updating spaceship theme"
 			curl https://raw.githubusercontent.com/denysdovhan/spaceship-zsh-theme/master/spaceship.zsh -o $HOME/.oh-my-zsh/custom/themes/spaceship.zsh-theme
-            ;;
-        esac
+			;;
+	esac
 }
 
 function install() {
-    local TARGET="$1"
+	local TARGET="$1"
 	echo -e "Do you want to install \033[1;33m$TARGET\033[0m [(Y)es/(n)o]:"
 	read line
 	if [[ "$line" == Y* ]] || [[ "$line" == y* ]] || [ -z "$line" ]; then
@@ -337,33 +337,33 @@ fi
 
 echo "Starting installation..."
 if [[ $# -eq 0 ]]; then
-    print_help_msg
-    exit 0
+	print_help_msg
+	exit 0
 elif [[ $# -gt 1 ]]; then
-    echo "You can only specify one target"
-    exit 1
+	echo "You can only specify one target"
+	exit 1
 else
-    if [[ $1 = all ]]; then
-        install autorandr
-        install dconf
-        install bash
-        install gdb
-        install git
-        install gtk
-        install i3-gaps
-        install ideavim
-        install polybar
-        install ranger
-        install readline
-        install terminator
-        install tmux
-        install vim
-        install neovim
-        install vimiv
-        install vrapper
-        install zathura
-        install zsh
-        install X
+	if [[ $1 = all ]]; then
+		install autorandr
+		install dconf
+		install bash
+		install gdb
+		install git
+		install gtk
+		install i3-gaps
+		install ideavim
+		install polybar
+		install ranger
+		install readline
+		install terminator
+		install tmux
+		install vim
+		install neovim
+		install vimiv
+		install vrapper
+		install zathura
+		install zsh
+		install X
 	elif [[ $1 == nogui ]]; then
 		install bash
 		install gdb
