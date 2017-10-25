d055f89e3927157f7a1b697f18cda74fa8008d3a
Thomas Pettinger <pettinger.thomas@gmail.com>
2017-10-25 19:39:56 +0200

    i3-gaps: improved startup procedure

--- i3-gaps/start_chrome_windows.sh
+++ i3-gaps/start_chrome_windows.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sleep 1
+sleep 3
 i3-msg "workspace 9"
 sleep 0.1
 google-chrome-beta --new-window messenger.com web.whatsapp.com cqse.slack.com "https://inbox.google.com/u/0/?pli=1" "https://inbox.google.com/u/1/?pli=1" &
@@ -7,8 +7,8 @@ sleep 8
 i3-msg "workspace 8"
 sleep 0.1
 google-chrome-beta --new-window calendar.google.com todoist.com &
-sleep 4
+sleep 6
 i3-msg "workspace 7"
 google-chrome-beta --new-window &
-sleep 4
+sleep 6
 i3-msg "workspace 1"
