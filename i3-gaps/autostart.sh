8097dbe795acc19562cac9ef8e48648c0975cb85
Thomas Pettinger <pettinger.thomas@gmail.com>
2017-11-15 00:06:47 +0100

    i3-gaps: startup custom script support + veracrypt adaptions

--- i3-gaps/autostart.sh
+++ i3-gaps/autostart.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 case $1 in
-	w )
-		terminator &
-		sleep 1
-		gpmdp &
-		sleep 3
-		i3-msg "workspace 10"
+	w ) # Work
+		i3-msg "workspace 10; exec gpmdp"
 		sleep 3
 		i3-msg "workspace 9"
 		sleep 0.1
@@ -18,15 +14,15 @@ case $1 in
 		i3-msg "workspace 7"
 		google-chrome-beta --new-window &
 		sleep 6
-		i3-msg "workspace 1"
-		sleep 0.1
-		terminator &
+		i3-msg "workspace 1; exec terminator"
+		sleep 1
+		if [ -e $HOME/.config/i3/custom_autostart.sh ]; then
+			$HOME/.config/i3/custom_autostart.sh w
+		fi
 		;;
-	p )
-		gpmdp &
+	p ) # Private
+		i3-msg "workspace 10; exec gpmdp"
 		sleep 3
-		i3-msg "workspace 10"
-		sleep 0.1
 		i3-msg "workspace 9"
 		sleep 0.1
 		google-chrome-beta --new-window messenger.com web.whatsapp.com &
@@ -35,5 +31,15 @@ case $1 in
 		sleep 0.1
 		google-chrome-beta --new-window calendar.google.com todoist.com &
 		sleep 6
-		i3-msg "workspace 1"
+		i3-msg "workspace 1; exec terminator"
+		sleep 1
+		if [ -e $HOME/.config/i3/custom_autostart.sh ]; then
+			$HOME/.config/i3/custom_autostart.sh p
+		fi
+		;;
 esac
+
+# Default
+if [ -e $HOME/.config/i3/custom_autostart.sh ]; then
+	$HOME/.config/i3/custom_autostart.sh d
+fi
