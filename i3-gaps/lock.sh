93bb91162c8f06592a570f36881cddb2effc8c55
Thomas Pettinger <pettinger.thomas@gmail.com>
2018-01-03 20:37:56 +0100

    i3-gaps: lock script

---
+++ i3-gaps/lock.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+i3lock -n -c 000000 -S=0 --insidevercolor=ffffffbb --insidewrongcolor=ff3a3a88 -insidecolor=ffffffbb --ringvercolor=ffffff88 --ringwrongcolor=ff3a3aff --ringcolor=ffffffff --linecolor=00000000 --keyhlcolor=474747ff --bshlcolor=ff3a3aff --veriftext="" --wrongtext="" --radius=16
