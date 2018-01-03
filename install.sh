4b3b67d3ea5b0c55f9f82ec1839338990c98761b
Thomas Pettinger <pettinger.thomas@gmail.com>
2018-01-03 20:38:57 +0100

    install: linking i3 lock script

--- install.sh
+++ install.sh
@@ -106,6 +106,7 @@ function link_config() {
 			mkdir -p $HOME/.config/i3
 			mkdir -p $HOME/Pictures
 			ln -snf $PWD/i3-gaps/config $HOME/.config/i3/config
+			ln -snf $PWD/i3-gaps/lock.sh $HOME/.config/i3/lock.sh
 			ln -snf $PWD/i3-gaps/autostart.sh $HOME/.config/i3/autostart.sh
 			ln -snf $PWD/i3-gaps/background.png $HOME/Pictures/i3-bg.png
 			;;
