c250daf46c98843008b6a3acae45ee739b9dacaa
Thomas Pettinger <pettinger.thomas@gmail.com>
2018-03-06 22:07:51 +0100

    i3-gaps: Clean up lock script

--- i3-gaps/lock.sh
+++ i3-gaps/lock.sh
@@ -14,14 +14,13 @@ TIME_HEIGHT=152
 TIME_POS_X=$((PRIMARY_SCREEN_X_OFFSET + PRIMARY_SCREEN_WIDTH - TIME_WIDTH - MARGIN))
 TIME_POS_Y=$((PRIMARY_SCREEN_Y_OFFSET + PRIMARY_SCREEN_HEIGHT - TIME_HEIGHT))
 LOCK_CMD="i3lock --nofork --screen=0 --color=000000
---insidevercolor=00000000 --insidewrongcolor=00000000 --insidecolor=00000000
---ringvercolor=2C2C2CFF --ringwrongcolor=0A0A0AFF --ringcolor=0A0A0AFF
---linecolor=00000000 --keyhlcolor=FFFFFFFF --bshlcolor=2C2C2CFF
---textcolor=00000000 --veriftext= --wrongtext=
---radius=$IND_RADIUS --indpos=$IND_POS_X:$IND_POS_Y
---clock --timecolor=FFFFFFFF --timestr=%H:%M --timepos=$TIME_POS_X:$TIME_POS_Y --time-align=2
---datecolor=666666FF --datestr=%A,\ %d\ %B\ %Y --date-align=2"
-echo $LOCK_CMD
+	--insidevercolor=00000000 --insidewrongcolor=00000000 --insidecolor=00000000
+	--ringvercolor=2C2C2CFF --ringwrongcolor=0A0A0AFF --ringcolor=0A0A0AFF
+	--linecolor=00000000 --keyhlcolor=FFFFFFFF --bshlcolor=2C2C2CFF
+	--textcolor=00000000 --veriftext= --wrongtext=
+	--radius=$IND_RADIUS --indpos=$IND_POS_X:$IND_POS_Y
+	--clock --timecolor=FFFFFFFF --timestr=%H:%M --timepos=$TIME_POS_X:$TIME_POS_Y --time-align=2
+	--datecolor=666666FF --datestr=%A,\ %d.\ %B\ %Y --date-align=2"
 
 DISPLAY_RE="([0-9]+)x([0-9]+)\\+([0-9]+)\\+([0-9]+)" # Regex to find display dimensions
 FOLDER=`pwd` # Current folder
