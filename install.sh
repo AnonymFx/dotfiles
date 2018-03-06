66d8a36c3300c3c0aa87fa875e08056835ffd7d8
Thomas Pettinger <pettinger@cqse.eu>
2018-03-06 23:52:15 +0100

    Install: Add imagemagick to i3 dependencies

--- install.sh
+++ install.sh
@@ -86,7 +86,7 @@ function get_package_list() {
 			return 0
 			;;
 		i3-gaps )
-			echo "i3-gaps i3lock-color dmenu rofi compton feh autorandr-git udevil gnome-keyring libnotify xdotool"
+			echo "i3-gaps i3lock-color dmenu rofi compton feh autorandr-git udevil gnome-keyring libnotify xdotool imagemagick"
 			return 0
 			;;
 		ideavim )
