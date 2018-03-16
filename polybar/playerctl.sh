ffff868ea2d3cde464bdadf2744d1f956a953adb
Thomas Pettinger <pettinger@cqse.eu>
2018-03-16 12:45:01 +0100

    Polybar: Allow longer artist names in playerctl script if title is quite short

--- polybar/playerctl.sh
+++ polybar/playerctl.sh
@@ -2,12 +2,20 @@
 playerstatus="$(playerctl status 2>/dev/null)"
 if [ "$playerstatus" == "Playing" ]; then
 	maxlen=$1
-	title=`exec playerctl metadata xesam:title 2>/dev/null`
-	artist=`exec playerctl metadata xesam:artist 2>/dev/null`
+	maxlen=$((maxlen - 3)) # Include the separator " - " in maxlen calculations
 
+	title=`exec playerctl metadata xesam:title 2>/dev/null`
+	titlelen=${#title}
+	artist=`exec playerctl metadata xesam:artist 2>/dev/null`
+	artistlen=${#artist}
+	
 	# Show max 1/3 of the length the author and on the rest the title
 	maxartistlen=$((maxlen/3))
-	artistlen=${#artist}
+	# Or, if the title is very short, allow a longer artist
+	if [ $((maxlen - titlelen)) > $maxartistlen ]; then
+		maxartistlen=$((maxlen - titlelen))
+	fi
+
 	if [[ $artistlen -gt $maxartistlen ]]; then
 		artistlen=$maxartistlen
 	fi
