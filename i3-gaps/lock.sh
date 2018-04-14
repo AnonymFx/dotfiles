c0c3f1eb82f507f99cd3f9d7891fa81b9c6a7fd5
Thomas Pettinger <pettinger.thomas@gmail.com>
2018-04-14 20:16:19 -0300

    i3-gaps: Adapt text color parameter to verifcolor and wrongcolor option changes

--- i3-gaps/lock.sh
+++ i3-gaps/lock.sh
@@ -9,15 +9,15 @@ IND_RADIUS=15
 MARGIN=24
 IND_POS_X=$((PRIMARY_SCREEN_X_OFFSET + IND_RADIUS + MARGIN))
 IND_POS_Y=$((PRIMARY_SCREEN_HEIGHT + PRIMARY_SCREEN_Y_OFFSET - IND_RADIUS - MARGIN))
-TIME_WIDTH=400
-TIME_HEIGHT=152
+TIME_WIDTH=0
+TIME_HEIGHT=50
 TIME_POS_X=$((PRIMARY_SCREEN_X_OFFSET + PRIMARY_SCREEN_WIDTH - TIME_WIDTH - MARGIN))
 TIME_POS_Y=$((PRIMARY_SCREEN_Y_OFFSET + PRIMARY_SCREEN_HEIGHT - TIME_HEIGHT))
 LOCK_CMD="i3lock --nofork --screen=0 --color=000000
 	--insidevercolor=00000000 --insidewrongcolor=00000000 --insidecolor=00000000
 	--ringvercolor=2C2C2CFF --ringwrongcolor=0A0A0AFF --ringcolor=0A0A0AFF
 	--linecolor=00000000 --keyhlcolor=FFFFFFFF --bshlcolor=2C2C2CFF
-	--textcolor=00000000 --veriftext= --wrongtext=
+	--wrongcolor=00000000 --verifcolor=00000000 --veriftext= --wrongtext=
 	--radius=$IND_RADIUS --indpos=$IND_POS_X:$IND_POS_Y
 	--clock --timecolor=FFFFFFFF --timestr=%H:%M --timepos=$TIME_POS_X:$TIME_POS_Y --time-align=2
 	--datecolor=666666FF --datestr=%A,\ %d.\ %B\ %Y --date-align=2"
