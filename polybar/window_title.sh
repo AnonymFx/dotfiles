58d5decdbc6b9c83213ec156a4ffc2ccf06baebc
Thomas Pettinger <pettinger@cqse.eu>
2018-09-28 15:27:26 +0200

    polybar: indicate that window title was shortened in window_title module

--- polybar/window_title.sh
+++ polybar/window_title.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
-window_title="$(xdotool getactivewindow getwindowname)"
+max_len=42
+window_title="$(xdotool getactivewindow getwindowname 2>/dev/null)" 
+if  [[ $? -gt 0 ]]; then
+	echo ""
+	exit
+fi
 
 window_title_short=$(echo "$window_title" | grep -oP ".*? -" | head -1)
 if [[ ! -z "$window_title_short" ]]; then
 	window_title="$window_title_short"
+	window_title=${window_title::-2}
 fi
 
-echo ${window_title::-2} | cut -c 1-42
+if [[ ${#window_title} -gt $max_len ]]; then
+	window_title="${window_title:0:$((max_len-1))}…"
+fi
+
+echo $window_title
 
