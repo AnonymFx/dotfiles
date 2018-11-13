c183c7505b0a4222ce3b9e17a3c244b7a9457340
Thomas Pettinger <pettinger@cqse.eu>
2018-11-13 08:17:17 +0100

    i3-gaps: fix bug where wrong window was resized when moving another window to front from scratchpad

--- i3-gaps/scratchpad.py
+++ i3-gaps/scratchpad.py
@@ -2,6 +2,7 @@
 
 import i3ipc
 import rofi
+from time import sleep
 
 i3 = i3ipc.Connection()
 menu = rofi.Rofi()
@@ -24,4 +25,4 @@ wins, names = get_windows()
 index, key = menu.select("Scratchpad",names)#,select="0 -theme /home/brian/.config/rofi/i3scratch.rasi")
 
 for i in range((index+1)*2-1):
-    i3.command("scratchpad show, resize set 1800 1012, move position center")
+    i3.command("exec i3-msg 'scratchpad show' && i3-msg 'resize set 1800 1012' && i3-msg 'move position center'")
