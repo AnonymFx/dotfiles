111d1ffc2390b4882e351184fc5bf1401bd445b8
Thomas Pettinger <pettinger@cqse.eu>
2019-01-07 10:42:56 +0100

    i3-gaps: Switch autostart to firefox

--- i3-gaps/autostart.sh
+++ i3-gaps/autostart.sh
@@ -2,30 +2,31 @@
 case $1 in
 	w ) # Work
 		i3-msg "exec --no-startup-id spotify & sleep 0.5 && i3-msg move scratchpad"
-		sleep 3
-		i3-msg "workspace 10"
-		sleep 0.1
-		google-chrome-beta --new-window messenger.com web.whatsapp.com cqse.slack.com "https://mail.google.com/mail/u/0" "https://mail.google.com/mail/u/1" &
-		sleep 8
-		i3-msg "workspace 9"
-		sleep 0.1
-		google-chrome-beta --new-window calendar.google.com/b/0 calendar.google.com/b/1 todoist.com 'https://dev.azure.com/apps-munichre/QualityTools/_sprints'
-		sleep 5
+		sleep 1
+
+		GTK_THEME=Arc firefox messenger.com web.whatsapp.com cqse.slack.com "https://mail.google.com/mail/u/0" "https://mail.google.com/mail/u/1" &
+		sleep 2
+		i3-msg "move window to workspace 10"
+
+		GTK_THEME=Arc firefox calendar.google.com/b/0 calendar.google.com/b/1 todoist.com 'https://dev.azure.com/apps-munichre/QualityTools/_sprints'
+		sleep 1
+		i3-msg "move window to workspace 9"
+
 		if [ -e $HOME/.config/i3/custom_autostart.sh ]; then
 			$HOME/.config/i3/custom_autostart.sh w
 		fi
 		;;
 	p ) # Private
 		i3-msg "exec --no-startup-id spotify & sleep 0.5 && i3-msg move scratchpad"
+
+		GTK_THEME=Arc firefox messenger.com web.whatsapp.com mail.google.com &
 		sleep 3
-		i3-msg "workspace 10"
-		sleep 0.1
-		google-chrome-beta --new-window messenger.com web.whatsapp.com mail.google.com &
-		sleep 5
-		i3-msg "workspace 9"
-		sleep 0.1
-		google-chrome-beta --new-window calendar.google.com/b/0 todoist.com &
-		sleep 5
+		i3-msg "move window to workspace 10"
+
+		GTK_THEME=Arc firefox calendar.google.com/b/0 todoist.com &
+		sleep 3
+		i3-msg "move window to workspace 9"
+
 		if [ -e $HOME/.config/i3/custom_autostart.sh ]; then
 			$HOME/.config/i3/custom_autostart.sh p
 		fi
