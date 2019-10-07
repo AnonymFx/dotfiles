44504f6bb38db7e955b2c8120cc02b9c4c46b8d7
Thomas Pettinger <pettinger@cqse.eu>
2019-10-07 15:09:08 +0200

    Switch from rofimoji to splatmoji

--- install.sh
+++ install.sh
@@ -106,7 +106,7 @@ function get_package_list() {
 					return 0
 					;;
 				i3-gaps )
-					echo "i3-gaps light-locker dmenu rofi compton feh autorandr-git udevil gnome-keyring libnotify xdotool imagemagick playerctl network-manager-applet insync polkit polkit-gnome xdg-user-dirs python python-rofi i3ipc-python rofimoji-git"
+					echo "i3-gaps light-locker dmenu rofi compton feh autorandr-git udevil gnome-keyring libnotify xdotool imagemagick playerctl network-manager-applet insync polkit polkit-gnome xdg-user-dirs python python-rofi i3ipc-python splatmoji-git"
 					return 0
 					;;
 				ideavim )
