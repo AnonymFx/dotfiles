7b76f338b15b2d70b14a1cb49b47da5474091a65
Thomas Pettinger <pettinger@cqse.eu>
2019-10-24 11:49:06 +0200

    Remove azure devops from organization script

--- scripts/bin/organization.sh
+++ scripts/bin/organization.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-firefox calendar.google.com/b/0 calendar.google.com/b/1 todoist.com 'https://dev.azure.com/apps-munichre/QualityTools/_sprints' &
+firefox calendar.google.com/b/0 calendar.google.com/b/1 todoist.com &
 sleep 1.25
 i3-msg 'move window to workspace 9'
