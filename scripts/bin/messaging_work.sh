babb65138565563b5765cc7e06f73cd9917de344
Thomas Pettinger <pettinger@cqse.eu>
2019-10-25 11:07:08 +0200

    Remove hangout chat from work messaging

--- scripts/bin/messaging_work.sh
+++ scripts/bin/messaging_work.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-firefox cqse.slack.com https://chat.google.com/u/1 https://web.whatsapp.com/ mail.google.com/mail/u/0 mail.google.com/mail/u/1 &
+firefox cqse.slack.com https://web.whatsapp.com/ mail.google.com/mail/u/0 mail.google.com/mail/u/1 &
 sleep 1.25
 i3-msg 'move window to workspace 10'
