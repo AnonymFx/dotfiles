171ea8884de7e2a4d825fd71e843bfb16e44260a
Thomas Pettinger <pettinger@cqse.eu>
2020-05-28 11:36:05 +0200

    Use default firefox profile for private scripts

--- scripts/bin/messaging.sh
+++ scripts/bin/messaging.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-firefox web.whatsapp.com cqse.slack.com mail.google.com/mail/u/0 mail.google.com/mail/u/1 &
+firefox -P default web.whatsapp.com cqse.slack.com mail.google.com/mail/u/0 mail.google.com/mail/u/1 &
 sleep 2
 i3-msg 'move window to workspace 10'
 
--- scripts/bin/organization.sh
+++ scripts/bin/organization.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-firefox calendar.google.com/b/0 calendar.google.com/b/1 todoist.com &
+firefox -P default calendar.google.com/b/0 calendar.google.com/b/1 todoist.com &
 sleep 2
 i3-msg 'move window to workspace 9'
