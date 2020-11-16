627ac3b0518cbdd35bb1fe71278286b6ce3275d3
Thomas Pettinger <pettinger.thomas@gmail.com>
2020-11-16 21:08:46 +0100

    i3-gaps: Change startup order of work autostart

--- i3-gaps/autostart.sh
+++ i3-gaps/autostart.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env sh
 case $1 in
 	w ) # Work
-		i3-msg "workspace 1" &&
-		keepassxc "$HOME/Drive_work/pettinger@cqse.kdbx" & sleep 1
-		terminator -x /usr/bin/zsh -i -c cqse
-
 		spotify &
 		sleep 1
 		i3-msg "move scratchpad"
 
 		$HOME/bin/messaging_work
-		sleep 2
+		sleep 1
 		i3-msg "move workspace 10"
 
 		$HOME/bin/organization_work
 		sleep 1
 		i3-msg "move workspace 9"
+
+		i3-msg "workspace 1" &&
+		keepassxc "$HOME/Drive_work/pettinger@cqse.kdbx" & sleep 1
+		terminator -x /usr/bin/zsh -i -c cqse &
+		sleep 1
 
 		if [ -e $HOME/.config/i3/custom_autostart.sh ]; then
 			$HOME/.config/i3/custom_autostart.sh w
