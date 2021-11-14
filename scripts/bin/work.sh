9ca46c0f65b7acefe62bccf63145ab70464d75e3
Thomas Pettinger <pettinger.thomas@gmail.com>
2021-11-14 17:40:28 +0100

    scripts: change browser in work script to chrome

--- scripts/bin/work.sh
+++ scripts/bin/work.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-firefox -P Work https://docs.google.com/spreadsheets/d/1ePvMGmlOzvlHFGOj3T6VuPkmOZdRXHgtq_PvDLLnAng/edit#gid=0 cqse.slack.com mail.google.com/mail/u/0 calendar.google.com todoist.com &
+google-chrome-stable https://docs.google.com/spreadsheets/d/1ePvMGmlOzvlHFGOj3T6VuPkmOZdRXHgtq_PvDLLnAng/edit#gid=0 cqse.slack.com mail.google.com/mail/u/0 calendar.google.com todoist.com &
