cbb0f48ea1a02b19dc465bdf6542d67e60785cdf
Thomas Pettinger <pettinger.thomas@gmail.com>
2021-11-24 12:01:04 +0100

    scripts: Add switching windows to x-clipboard-input script

--- scripts/bin/x-clipboard-input.sh
+++ scripts/bin/x-clipboard-input.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+qdbus-qt5 org.kde.kglobalaccel /component/kwin invokeShortcut "Walk Through Windows"
 sh -c 'sleep 0.2; xdotool type "$(xclip -o -selection clipboard)"'
 
 echo '' | xclip -selection clipboard -in
