3b862c71c4271bb2cf2ddc7e7c6c92b4b08ffe86
Thomas Pettinger <pettinger@cqse.eu>
2024-02-15 18:26:40 +0100

    Scripts: exifrename handle if multiple photos have the same timestamp

--- scripts/bin/exifrename.sh
+++ scripts/bin/exifrename.sh
@@ -3,13 +3,15 @@ for file in $@; do
 	datetimeoriginal="$(exiftool -DateTimeOriginal -S -d %Y%m%d_%H%M%S "$file" | cut -d " " -f 2)"
 	filename=$(basename -- "$file")
 	extension="${filename##*.}"
-	newfilename="$datetimeoriginal.$extension"
+	newfilename="$datetimeoriginal"
 	if [[ ! -z $datetimeoriginal ]]; then
-		if [[ ! -e "$newfilename" ]]; then
-			mv "$file" "$newfilename"
-		else
-			echo Cannot move $file: $newfilename already exists
-		fi
+		newfilenamewithsuffix="$newfilename"
+		suffix=1
+		while [[ -e "$newfilenamewithsuffix.$extension" ]]; do
+			newfilenamewithsuffix="$newfilename-$suffix"
+			suffix=$((suffix + 1))
+		done
+		mv "$file" "$newfilenamewithsuffix.$extension"
 	else
 		echo "Cannot move $file: No DateTimeOriginal set"
 	fi
