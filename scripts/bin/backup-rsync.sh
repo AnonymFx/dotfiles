e723d86038634d8754248419f1f52d02a44fd7b3
Thomas Pettinger <pettinger.thomas@gmail.com>
2025-06-29 15:09:04 +0200

    backup-rsync: Try to fix deletion of files not possible with umlauts

--- scripts/bin/backup-rsync.sh
+++ scripts/bin/backup-rsync.sh
@@ -2,6 +2,5 @@
 if [[ $# < 2 ]]; then
 	echo Please provide a source and destination
 fi
-# rsync archive mode without l (copy symlinks as symlinks), d (device files), p (permissions) but with --update and --info
-rsync --recursive --times --group --owner --update --info=progress2 "$1/" "$2"
-find "$2" -type f -iname "._*"
+rsync --recursive --info=progress2 "$1/" "$2"
+find "$2" -type f -name "._*" -delete
