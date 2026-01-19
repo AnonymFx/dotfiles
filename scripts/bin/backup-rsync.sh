a761ab9ae1b1997df1bbcb187edb58c4623bd53c
Thomas Pettinger <pettinger.thomas@gmail.com>
2026-01-19 09:12:27 +0100

    backup-rsync: Keep metadata files

--- scripts/bin/backup-rsync.sh
+++ scripts/bin/backup-rsync.sh
@@ -2,5 +2,9 @@
 if [[ $# < 2 ]]; then
 	echo Please provide a source and destination
 fi
-rsync --recursive --info=progress2 "$1/" "$2"
-find "$2" -type f -name "._*" -delete
+rsync --archive --info=progress2 "$1/" "$2"
+# find "$2" -type f -name "._*" -print0 | while IFS= read -r -d '' file;
+# do
+# 	# TODO: To fix umlauts, go to the folder and just do a rm ._* For some reason that doesn't work with a folder before the glob pattern
+# 	[[ -f "$file" ]] && rm -f "$file"
+# done
